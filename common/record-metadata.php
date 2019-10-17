ce63168a17241e70161112e59a86563f8e5bd740
Joseph Anderson <joseph_anderson@fitnyc.edu>
2019-10-17 14:40:31 -0400

    fix to heading links

--- common/record-metadata.php
+++ common/record-metadata.php
@@ -9,7 +9,7 @@
     		<div id="<?php echo text_to_id(html_escape("$setName $elementName")); ?>" class="element">
     			<h3><?php echo html_escape(__($elementName)); ?></h3>
     			<?php foreach ($elementInfo['texts'] as $text): ?>
-    				<div class="element-text"><?php echo heading_links($elementName, $text); ?></div>
+    				<div class="element-text"><?php echo heading_links($setName, $elementName, $text); ?></div>
     			<?php endforeach; ?>
     		</div><!-- end element -->
     	<?php else: ?>
