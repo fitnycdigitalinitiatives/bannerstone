8175064c1c1f3e11bb6513d0676ea01707fac6cd
Joseph Anderson <joseph_anderson@fitnyc.edu>
2023-06-07 15:12:43 -0400

    update email

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php echo head(array('bodyid'=>'home')); ?>
+<?php echo head(array('bodyid' => 'home')); ?>
 
 <div class="container-fluid" id="banner">
   <div class="row justify-content-md-center">
@@ -6,22 +6,26 @@
       <img src="<?php echo img('grid_2-1.jpg'); ?>" class="img-fluid" alt="Bannerstone AMNH DN/128 view 1">
     </div>
     <div class="col-md-auto" id="small-img">
-        <div class="row">
-          <div class="col-md-auto">
-            <img src="<?php echo img('grid_2-2.jpg'); ?>" class="img-fluid" id="small-top" alt="Bannerstone AMNH DN/128 view 2">
-          </div>
+      <div class="row">
+        <div class="col-md-auto">
+          <img src="<?php echo img('grid_2-2.jpg'); ?>" class="img-fluid" id="small-top"
+            alt="Bannerstone AMNH DN/128 view 2">
         </div>
-        <div class="row">
-          <div class="col-md-auto">
-            <img src="<?php echo img('grid_2-3.jpg'); ?>" class="img-fluid" id="small-middle" alt="Bannerstone AMNH DN/128 view 3">
-          </div>
+      </div>
+      <div class="row">
+        <div class="col-md-auto">
+          <img src="<?php echo img('grid_2-3.jpg'); ?>" class="img-fluid" id="small-middle"
+            alt="Bannerstone AMNH DN/128 view 3">
         </div>
-        <div class="row">
-          <div class="col-md-auto">
-            <img src="<?php echo img('grid_2-4.jpg'); ?>" class="img-fluid" id="small-bottom" alt="Bannerstone AMNH DN/128 view 4">
-            <a class="d-block text-right mt-2 text-dark" id="info" href="https://bannerstone.fitnyc.edu/items/show/112"><i class="material-icons align-bottom">info</i> AMNH DN/128</a>
-          </div>
+      </div>
+      <div class="row">
+        <div class="col-md-auto">
+          <img src="<?php echo img('grid_2-4.jpg'); ?>" class="img-fluid" id="small-bottom"
+            alt="Bannerstone AMNH DN/128 view 4">
+          <a class="d-block text-right mt-2 text-dark" id="info" href="https://bannerstone.fitnyc.edu/items/show/112"><i
+              class="material-icons align-bottom">info</i> AMNH DN/128</a>
         </div>
+      </div>
     </div>
   </div>
 </div>
@@ -32,42 +36,104 @@
       <?php fire_plugin_hook('public_home', array('view' => $this)); ?>
       <section id="introduction">
         <h1 class="text-center mb-4">Introduction</h1>
-        <p>The <strong>A</strong>rchaic <strong>B</strong>annerstone <strong>P</strong>roject is a resource for the study of the aesthetically complex, anomalous ancient Native American lithics known as bannerstones. Finely composed, carved, and polished works are usually associated with luxury items for a ruling elite.  Bannerstones, however, were made within distinctly egalitarian societies raising questions about who made them, who were they made for, and for what purposes were they made.</p>
-        <p>To engage with these and other questions we have created this open-source website to bring bannerstone awareness and study to the broadest range of people. Included on this site are a bannerstone typology, high-quality images, a glossary, a bibliography, and a vast array of out-of-print or difficult to find texts relevant to bannerstone study. Also included are suggested metrics to better standardize the study of bannerstones in public and private collections as well as a suggested guide for photographic practices.</p>
-        <p>Learning to see, study, and value the art and technology of bannerstones will bring us closer to the lived lives of ancient Native Americans of Eastern North America. This study, awareness, and appreciation of Ancient Native Americans in turn can open ways of seeing and respecting the vast history that links the past to the present.</p>
+        <p>The <strong>A</strong>rchaic <strong>B</strong>annerstone <strong>P</strong>roject is a resource for the
+          study of the aesthetically complex, anomalous ancient Native American lithics known as bannerstones. Finely
+          composed, carved, and polished works are usually associated with luxury items for a ruling elite.
+          Bannerstones, however, were made within distinctly egalitarian societies raising questions about who made
+          them, who were they made for, and for what purposes were they made.</p>
+        <p>To engage with these and other questions we have created this open-source website to bring bannerstone
+          awareness and study to the broadest range of people. Included on this site are a bannerstone typology,
+          high-quality images, a glossary, a bibliography, and a vast array of out-of-print or difficult to find texts
+          relevant to bannerstone study. Also included are suggested metrics to better standardize the study of
+          bannerstones in public and private collections as well as a suggested guide for photographic practices.</p>
+        <p>Learning to see, study, and value the art and technology of bannerstones will bring us closer to the lived
+          lives of ancient Native Americans of Eastern North America. This study, awareness, and appreciation of Ancient
+          Native Americans in turn can open ways of seeing and respecting the vast history that links the past to the
+          present.</p>
       </section>
       <section id="what">
         <h1 class="text-center mb-4">What are Bannerstones?</h1>
-        <p>Native Americans carved and carefully drilled holes through the center of these stones leading early 20th century archaeologists to believe they were meant to be placed on staffs as banners or emblems, thus the name <em>bannerstone</em>. Currently there is significant uncertainty about why these stones were made and what role they played in the lives of ancient Native Americans. What is certain is that bannerstones as a distinct category of stone carving are amongst the most aesthetically complex artifacts made by North Americans during the Archaic period.</p>
-        <p>Whether local to them or traded from distant areas, stones were chosen by nomadic as well as settled Native Americans who worked the carved surface in relation to natural striations or other inherent visual qualities. Like axe heads, bannerstones were made by pecking, grinding and polishing lithic surfaces. This technology of lithic manufacture was practiced throughout North, South, and Central America from the Archaic to the Modern period (8000 BCE – AD1900), bannerstones, however, were only made in Eastern United States during the Archaic period. Most lithics made in this manner are of hard igneous or metamorphic rock suitable for well-known purposes of hammering or cutting. Bannerstones, however, are made up of a vast array of stones from hard igneous rock to metamorphic and even softer sedimentary rock.</p>
-        <p>Since bannerstones were not used for hammering or any kind of percussive practice, the choice of stone for their making was either about weight and / or visual appearance.  Bannerstones were also carved into over twenty distinctive, expressive shapes pointing to the importance of aesthetic and conceptual concerns in their making, meaning, and uses. </p>
+        <p>Native Americans carved and carefully drilled holes through the center of these stones leading early 20th
+          century archaeologists to believe they were meant to be placed on staffs as banners or emblems, thus the name
+          <em>bannerstone</em>. Currently there is significant uncertainty about why these stones were made and what
+          role they played in the lives of ancient Native Americans. What is certain is that bannerstones as a distinct
+          category of stone carving are amongst the most aesthetically complex artifacts made by North Americans during
+          the Archaic period.
+        </p>
+        <p>Whether local to them or traded from distant areas, stones were chosen by nomadic as well as settled Native
+          Americans who worked the carved surface in relation to natural striations or other inherent visual qualities.
+          Like axe heads, bannerstones were made by pecking, grinding and polishing lithic surfaces. This technology of
+          lithic manufacture was practiced throughout North, South, and Central America from the Archaic to the Modern
+          period (8000 BCE – AD1900), bannerstones, however, were only made in Eastern United States during the Archaic
+          period. Most lithics made in this manner are of hard igneous or metamorphic rock suitable for well-known
+          purposes of hammering or cutting. Bannerstones, however, are made up of a vast array of stones from hard
+          igneous rock to metamorphic and even softer sedimentary rock.</p>
+        <p>Since bannerstones were not used for hammering or any kind of percussive practice, the choice of stone for
+          their making was either about weight and / or visual appearance. Bannerstones were also carved into over
+          twenty distinctive, expressive shapes pointing to the importance of aesthetic and conceptual concerns in their
+          making, meaning, and uses. </p>
       </section>
       <section id="images">
         <h1 class="text-center mb-4">Images and Resources on this Website</h1>
-        <p>All images on the ABP (Archaic Bannerstone Project) under the Bannerstone Tab may be freely downloaded, shared, and reused for non-commercial purposes with proper attribution. Please attribute photographs to Anna Blume (unless otherwise indicated) and review the separate credit line details under Collections.</p>
-        <p>Under the Bannerstone tab all images are available alphabetized by collection. Under the Collection Tab you can find notes on these specific collections as well as all images and metadata from that collection. Under the Typology Tab you will be able to search by type. By clicking on each bannerstone image you will open up the full suite of images and metadata for this stone. Images from each particular collection will be identified by the name of the collection and the catalog or accession number. For instance (AMNH 1/1821) refers to a specific quartz butterfly bannerstone in the American Museum of Natural History. All web essays and resources under the Resource Tab are also available for download.</p>
-        <p>For publishing questions or general suggestions regarding the ABP, contact <a href="mailto:bannerstone@fitnyc.edu">bannerstone@fitnyc.edu</a>.</p>
+        <p>All images on the ABP (Archaic Bannerstone Project) under the Bannerstone Tab may be freely downloaded,
+          shared, and reused for non-commercial purposes with proper attribution. Please attribute photographs to Anna
+          Blume (unless otherwise indicated) and review the separate credit line details under Collections.</p>
+        <p>Under the Bannerstone tab all images are available alphabetized by collection. Under the Collection Tab you
+          can find notes on these specific collections as well as all images and metadata from that collection. Under
+          the Typology Tab you will be able to search by type. By clicking on each bannerstone image you will open up
+          the full suite of images and metadata for this stone. Images from each particular collection will be
+          identified by the name of the collection and the catalog or accession number. For instance (AMNH 1/1821)
+          refers to a specific quartz butterfly bannerstone in the American Museum of Natural History. All web essays
+          and resources under the Resource Tab are also available for download.</p>
+        <p>For publishing questions or general suggestions regarding the ABP, contact <a
+            href="mailto:bannerstone@fitnyc.edu">bannerstone@fitnyc.edu</a>.</p>
       </section>
       <section id="team">
         <h1 class="text-center mb-4">Project Team</h1>
         <h2>Project Director</h2>
         <p class="mb-2">
-          <strong>Anna Blume</strong>, PhD, Professor of the History of Art at the Fashion Institute of Technology, State University of New York. Professor Blume received her PhD in the History of Art at Yale University in 1997. She has been researching, writing, and teaching courses on indigenous art of the Americas for the past twenty years. Her recent work includes a study of Maya concepts of zero, published by the <em>American Philosophical Society</em> in 2009, and a study of ancient monumental art along the Mississippi published in <em>RES: A Journal of Anthropology and Aesthetics</em> in 2018. Professor Blume began an ongoing in-depth study of Bannerstones in 2016 working with the American Museum of Natural History collection of 462 stones. The Archaic Bannerstone Project was conceived by her as a way to bring these important carved stones to as broad a public as possible, to reveal the deep history of the creative imagination of the indigenous people of eastern North America.
+          <strong>Anna Blume</strong>, PhD, Professor of the History of Art at the Fashion Institute of Technology,
+          State University of New York. Professor Blume received her PhD in the History of Art at Yale University in
+          1997. She has been researching, writing, and teaching courses on indigenous art of the Americas for the past
+          twenty years. Her recent work includes a study of Maya concepts of zero, published by the <em>American
+            Philosophical Society</em> in 2009, and a study of ancient monumental art along the Mississippi published in
+          <em>RES: A Journal of Anthropology and Aesthetics</em> in 2018. Professor Blume began an ongoing in-depth
+          study of Bannerstones in 2016 working with the American Museum of Natural History collection of 462 stones.
+          The Archaic Bannerstone Project was conceived by her as a way to bring these important carved stones to as
+          broad a public as possible, to reveal the deep history of the creative imagination of the indigenous people of
+          eastern North America.
         </p>
         <p>
-          <a href="http://www.fitnyc.edu/art-history/faculty/anna-blume.php">Professor Blume's FIT Faculty Page</a>
+          <a href="https://www.fitnyc.edu/creative-nexus/faculty/directory/blume-anna.php">Professor Blume's FIT Faculty
+            Page</a>
         </p>
         <h2>Project Developer</h2>
         <p>
-          <strong>Joseph Anderson</strong>, MLIS, Assistant Professor, Digital Initiatives Librarian at the Fashion Institute of Technology, State University of New York. Professor Anderson is responsible for overseeing and directing library initiatives involving both digital content and metadata.  He has developed and currently oversees <em>Archive on Demand</em>, FIT’s online video resource; <em>SPARC Connect</em>, the Library’s Special Collections and Archives (SPARC) archival management tool; and <em>SPARC Digital</em>, an online exhibition platform and image database. In addition, he has collaborated with FIT Faculty on a number of digital humanities projects.
+          <strong>Joseph Anderson</strong>, MLIS, Assistant Professor, Digital Initiatives Librarian at the Fashion
+          Institute of Technology, State University of New York. Professor Anderson is responsible for overseeing and
+          directing library initiatives involving both digital content and metadata. He has developed and currently
+          oversees <em>Archive on Demand</em>, FIT’s online video resource; <em>SPARC Connect</em>, the Library’s
+          Special Collections and Archives (SPARC) archival management tool; and <em>SPARC Digital</em>, an online
+          exhibition platform and image database. In addition, he has collaborated with FIT Faculty on a number of
+          digital humanities projects.
         </p>
         <h2>Metadata Coordinator</h2>
         <p>
-          <strong>Molly Schoen</strong>, MLIS, Visual Resources Curator at the Fashion Institute of Technology, State University of New York. Her role in the Archaic Bannerstone Project involves creating and updating image metadata records, as well as general editing of text. In her position as Visual Resources Curator, she assists History of Art faculty with their research, technology, and image needs, and she maintains a large collection of art images for FITDIL, the FIT Digital Image Library. She has also published in several journals and books, including <em>The Handbook of Art and Design Librarianship</em>, second edition.
+          <strong>Molly Schoen</strong>, MLIS, Visual Resources Curator at the Fashion Institute of Technology, State
+          University of New York. Her role in the Archaic Bannerstone Project involves creating and updating image
+          metadata records, as well as general editing of text. In her position as Visual Resources Curator, she assists
+          History of Art faculty with their research, technology, and image needs, and she maintains a large collection
+          of art images for FITDIL, the FIT Digital Image Library. She has also published in several journals and books,
+          including <em>The Handbook of Art and Design Librarianship</em>, second edition.
         </p>
         <h2>Digital Imaging Specialist</h2>
         <p>
-          <strong>Nanja Andriananjason</strong>, BFA, Visual Resources Technologist at the Fashion Institute of Technology, State University of New York. He works closely with the Visual Resources Curator in digitizing teaching materials for the History of Art Department faculty members, as well as images for the Art History Collection on FITDIL. His decades-old expertise in color correction and digital manipulation played an important role in assisting the Project Director with fine tuning all the images in The Archaic Bannerstone Project.
+          <strong>Nanja Andriananjason</strong>, BFA, Visual Resources Technologist at the Fashion Institute of
+          Technology, State University of New York. He works closely with the Visual Resources Curator in digitizing
+          teaching materials for the History of Art Department faculty members, as well as images for the Art History
+          Collection on FITDIL. His decades-old expertise in color correction and digital manipulation played an
+          important role in assisting the Project Director with fine tuning all the images in The Archaic Bannerstone
+          Project.
         </p>
       </section>
     </div>
@@ -93,4 +159,4 @@
 
 
 
-<?php echo foot(array('bodyid'=>'home')); ?>
+<?php echo foot(array('bodyid' => 'home')); ?>
